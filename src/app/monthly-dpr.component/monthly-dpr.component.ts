c8e33468e8785dcda7c27909a4f95c1e8f5515bd
Adan op <Adanop01@gmail.com>
2025-09-27 10:10:37 +0530

    gitadded

--- src/app/monthly-dpr.component/monthly-dpr.component.ts
+++ src/app/monthly-dpr.component/monthly-dpr.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
 import { FormsModule } from '@angular/forms';
-import { DPRTask, DPRKPI,DPRReview,ProofhubTaskDto,DPRComment } from '../models/task.model';
+import { DPRTask, DPRKPI, DPRReview, ProofhubTaskDto, DPRComment } from '../models/task.model';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Api } from '../services/api';
 import Swal from 'sweetalert2';
-import { ToastrService } from 'ngx-toastr'; 
-
-
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-monthly-dpr',
@@ -45,15 +43,10 @@ import { ToastrService } from 'ngx-toastr';
     ]),
   ],
 })
-
-
-
-
 export class MonthlyDprComponent {
+  monthYear = '';
 
-  monthYear =  "";
-
-  EmailID = "";
+  EmailID = '';
 
   showTaskDetails = true;
   showKpiDetails = true;
@@ -63,7 +56,7 @@ export class MonthlyDprComponent {
   summaryText = '';
   showModal = false;
 
-  managementRemarks = "";
+  managementRemarks = '';
 
   ApprovalStatus = '';
 
@@ -84,8 +77,6 @@ export class MonthlyDprComponent {
   overallScore = 0;
   dprid = 0;
 
-
-
   tasks: DPRTask[] = [
     {
       taskName: '',
@@ -93,7 +84,7 @@ export class MonthlyDprComponent {
       estimatedHours: 0,
       actualHours: 0,
       productivity: 0,
-      selected: false
+      selected: false,
     },
   ];
 
@@ -104,7 +95,7 @@ export class MonthlyDprComponent {
       START_DATE: '',
       DUE_DATE: '',
       ESTIMATED_HOURS: '',
-      LOGGED_HOURS: ''
+      LOGGED_HOURS: '',
     },
   ];
 
@@ -116,27 +107,26 @@ export class MonthlyDprComponent {
       kpiId: 0,
       dprId: 0,
       employeeId: '',
-      kpiMasterId: 0
+      kpiMasterId: 0,
     },
   ];
 
-
-  remarksHistory: DPRComment[] = [{
+  remarksHistory: DPRComment[] = [
+    {
       commentId: 0,
-      hodId: "John Smith (HOD)",
-      commentText: "Employee has shown excellent performance this month with high quality deliverables.",
-      commentType: "APPROVE",
+      hodId: 'John Smith (HOD)',
+      commentText:
+        'Employee has shown excellent performance this month with high quality deliverables.',
+      commentType: 'APPROVE',
       CREATEDAT: new Date('2025-06-15 14:32'),
-  }];
+    },
+  ];
 
-  
+  constructor(private api: Api) {}
 
-    constructor(private api: Api) {}
-    
-    // constructor(private api: Api,private toastr: ToastrService) {} 
+  // constructor(private api: Api,private toastr: ToastrService) {}
 
-  ngOnInit() {    
-
+  ngOnInit() {
     this.setPreviousMonthYear();
 
     this.loadKPIs();
@@ -151,7 +141,6 @@ export class MonthlyDprComponent {
     }
 
     //this.GetDPREmployeeReviewDetails(4);
-
   }
 
   toggleTaskDetails() {
@@ -181,7 +170,7 @@ export class MonthlyDprComponent {
       estimatedHours: 0,
       actualHours: 0,
       productivity: 0,
-      selected: false
+      selected: false,
     });
   }
 
@@ -193,22 +182,18 @@ export class MonthlyDprComponent {
       kpiId: 0,
       dprId: 0,
       employeeId: '',
-      kpiMasterId: 0
+      kpiMasterId: 0,
     });
   }
-
- 
 
   closeModal() {
     this.showModal = false;
     this.summaryText = '';
   }
-  
 
   SubmitReview() {
-
     this.ApprovalStatus = 'S';
-    this.saveEmployeeDetails();  
+    this.saveEmployeeDetails();
   }
 
   saveDraft() {
@@ -217,182 +202,153 @@ export class MonthlyDprComponent {
   }
 
   ApproveReview() {
-    this.ApprovalStatus = 'A'; 
+    this.ApprovalStatus = 'A';
     this.HODReviewUpdate();
   }
 
   ReWorkReview() {
-    this.ApprovalStatus = 'R'; 
+    this.ApprovalStatus = 'R';
     this.HODReviewUpdate();
   }
 
   private HODReviewUpdate() {
-
     console.log(this.kpis);
 
-    this.dprid = 4; 
+    this.dprid = 4;
     const review: DPRReview = {
       employeeId: this.empId,
-      status: this.ApprovalStatus,       
-      hodId: 'Vinod',               
-      scoreQuality: Number (+this.quality),
+      status: this.ApprovalStatus,
+      hodId: 'Vinod',
+      scoreQuality: Number(+this.quality),
       scoreTimeliness: Number(+this.timeliness),
       scoreInitiative: Number(+this.initiative),
       scoreOverall: Number(+this.overallScore),
       remarks: this.managementRemarks,
-      dprid: this.dprid,         
+      dprid: this.dprid,
 
-      kpiList: this.kpis.map(t => ({       
-          kpiId: Number(t.kpiId),
-          dprId: Number(this.dprid),
-          employeeId: t.employeeId,
-          kpiMasterId: t.kpiMasterId,
-          kpiValue: Number(t.kpiValue),
-          remarks: t.remarks,
-          kpiDescription: t.kpiDescription
+      kpiList: this.kpis.map((t) => ({
+        kpiId: Number(t.kpiId),
+        dprId: Number(this.dprid),
+        employeeId: t.employeeId,
+        kpiMasterId: t.kpiMasterId,
+        kpiValue: Number(t.kpiValue),
+        remarks: t.remarks,
+        kpiDescription: t.kpiDescription,
       })),
-      
     };
 
     this.api.updateDPRReview(review).subscribe({
       next: (res: any) => {
-        
-        if(res.success){
-
+        if (res.success) {
           Swal.fire({
             icon: 'success',
             title: 'Operation successful',
             text: res.message,
           });
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Failed to update DPR: ' + res.message,
+          });
+        }
 
-        }
-        else{
-            Swal.fire({
-              icon: 'error',
-              title: 'Oops...',
-              text: 'Failed to update DPR: ' + res.message,
-            });
-        }
-        
         console.log(res);
       },
       error: (err) => {
         alert(JSON.stringify(err.error));
-      }
+      },
     });
-  
   }
 
-
- saveEmployeeDetails() {
-
-
+  saveEmployeeDetails() {
     const review: DPRReview = {
-      
-    employeeId: this.empId,                
-    month: new Date().getMonth() + 1,      
-    year: new Date().getFullYear(),        
-    workedHours: Number(this.WorkedHours), 
-    achievements: this.achievements || '', 
-    challenges: this.challenges || '',     
-    supportNeeded: this.supportNeeded || '', 
-    status: this.ApprovalStatus || '', 
-    hodId: this.reportingTo || 'Vinod',     
-    tasksList: this.tasks.map(t => ({       
-      taskName: t.taskName,
-      description: t.description,
-      actualHours: Number(t.actualHours),
-      estimatedHours: Number(t.estimatedHours),
-      productivity: Number(t.productivity)
-    })),
-      
+      employeeId: this.empId,
+      month: new Date().getMonth() + 1,
+      year: new Date().getFullYear(),
+      workedHours: Number(this.WorkedHours),
+      achievements: this.achievements || '',
+      challenges: this.challenges || '',
+      supportNeeded: this.supportNeeded || '',
+      status: this.ApprovalStatus || '',
+      hodId: this.reportingTo || 'Vinod',
+      tasksList: this.tasks.map((t) => ({
+        taskName: t.taskName,
+        description: t.description,
+        actualHours: Number(t.actualHours),
+        estimatedHours: Number(t.estimatedHours),
+        productivity: Number(t.productivity),
+      })),
     };
 
-    console.log("review"+review);
+    console.log('review' + review);
 
     this.api.insertDpr(review).subscribe({
       next: (res) => {
-        
         Swal.fire({
-            icon: 'success',
-            title: 'Operation successful',
-            text: res.message,
+          icon: 'success',
+          title: 'Operation successful',
+          text: res.message,
         });
-
       },
       error: (err) => {
-     
-      Swal.fire({
-        icon: 'error',
-        title: 'Validation Failed',
-        text: JSON.stringify(err.error) 
-      });
-      }
-
+        Swal.fire({
+          icon: 'error',
+          title: 'Validation Failed',
+          text: JSON.stringify(err.error),
+        });
+      },
     });
-
   }
-
 
   GetProofHubTask() {
     this.showModal = true;
 
     this.getUserProofhubTasks();
 
-    this.Proofhubtasks.forEach(task => {
+    this.Proofhubtasks.forEach((task) => {
       task.selected = false;
     });
-
   }
 
   getUserProofhubTasks() {
+    const email = this.EmailID || '';
+    const today = new Date();
 
-   
+    const startDate = new Date(today.getFullYear(), today.getMonth(), 1);
+    const endDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
 
-  const email = this.EmailID || '';
-  const today = new Date();
+    const startDateString = startDate.toISOString().split('T')[0]; // '2025-08-01'
+    const endDateString = endDate.toISOString().split('T')[0]; // '2025-08-31'
 
+    this.api.GetUserProofhubTasks(email, startDateString, endDateString).subscribe({
+      next: (res) => {
+        console.log('Proofhub tasks response:', res);
 
-  const startDate = new Date(today.getFullYear(), today.getMonth(), 1);
-  const endDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-
-  const startDateString = startDate.toISOString().split('T')[0];  // '2025-08-01'
-  const endDateString = endDate.toISOString().split('T')[0];      // '2025-08-31'
-
-  this.api.GetUserProofhubTasks(email, startDateString, endDateString).subscribe({
-    next: (res) => {
-      console.log('Proofhub tasks response:', res);
-      
-      this.Proofhubtasks = (res.data || []).map((task: any) => ({
-        TASK_ID: task.tasK_ID,
-        TASK_TITLE: task.tasK_TITLE,
-        TASK_DESCRIPTION: task.tasK_DESCRIPTION,
-        START_DATE: task.starT_DATE,
-        DUE_DATE: task.duE_DATE,
-        ESTIMATED_HOURS: task.estimateD_HOURS,
-        LOGGED_HOURS: task.loggeD_HOURS,
-        selected: false
-      }));
-
-
-    },
-    error: (err) => {
-      console.error('Error fetching tasks:', err);
-    }
-  });
-
+        this.Proofhubtasks = (res.data || []).map((task: any) => ({
+          TASK_ID: task.tasK_ID,
+          TASK_TITLE: task.tasK_TITLE,
+          TASK_DESCRIPTION: task.tasK_DESCRIPTION,
+          START_DATE: task.starT_DATE,
+          DUE_DATE: task.duE_DATE,
+          ESTIMATED_HOURS: task.estimateD_HOURS,
+          LOGGED_HOURS: task.loggeD_HOURS,
+          selected: false,
+        }));
+      },
+      error: (err) => {
+        console.error('Error fetching tasks:', err);
+      },
+    });
   }
 
   setPreviousMonthYear(): void {
-    const currentDate = new Date(); 
-    currentDate.setMonth(currentDate.getMonth() - 1); 
+    const currentDate = new Date();
+    currentDate.setMonth(currentDate.getMonth() - 1);
 
-    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long' }; 
-    this.monthYear = currentDate.toLocaleDateString('en-US', options); 
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long' };
+    this.monthYear = currentDate.toLocaleDateString('en-US', options);
   }
-
-  
-
 
   calculateProductivity(task: any) {
     if (task.estimatedHours && task.actualHours) {
@@ -402,52 +358,44 @@ export class MonthlyDprComponent {
     }
   }
 
- loadKPIs(): void {
-  this.api.GetActiveKPIs(this.department).subscribe(
-    (response: any) => {
-      if (response && response.success && response.data) {
+  loadKPIs(): void {
+    this.api.GetActiveKPIs(this.department).subscribe(
+      (response: any) => {
+        if (response && response.success && response.data) {
+          console.log('Loaded KPIs:', response.data);
 
-        console.log('Loaded KPIs:', response.data);
-
-        this.kpis = response.data.map((item: any) => ({
-          kpiMasterId: item.kpiid,        
-          kpiDescription: item.kpiname,   
-          kpiValue: 0,                    
-          remarks: '',
-          kpiId: 0,
-          dprId: 0,
-          employeeId: this.empId                      
-        }));
-      } else {
-        this.kpis = [];
+          this.kpis = response.data.map((item: any) => ({
+            kpiMasterId: item.kpiid,
+            kpiDescription: item.kpiname,
+            kpiValue: 0,
+            remarks: '',
+            kpiId: 0,
+            dprId: 0,
+            employeeId: this.empId,
+          }));
+        } else {
+          this.kpis = [];
+        }
+      },
+      (error) => {
+        console.error('Error loading KPIs:', error);
       }
-    },
-    (error) => {
-      console.error('Error loading KPIs:', error);
-    }
-  );
-}
-
-
-
+    );
+  }
 
   generateSummary() {
-  
-    const selectedTasks = this.Proofhubtasks.filter(t => t.selected);
+    const selectedTasks = this.Proofhubtasks.filter((t) => t.selected);
 
     if (selectedTasks.length === 0) {
-
       Swal.fire({
         icon: 'error',
         title: 'Operation Failed',
-        text: 'Please select at least one task!' 
+        text: 'Please select at least one task!',
       });
 
       return;
-
     }
 
-    
     this.api.summarizeTasks(selectedTasks).subscribe({
       next: (res) => {
         this.summaryText = res.summary;
@@ -455,21 +403,16 @@ export class MonthlyDprComponent {
       error: (err) => {
         console.error(err);
         alert('Error generating summary');
-      }
+      },
     });
-   
-    
-
   }
-
-
 
   GetDPREmployeeReviewDetails(dprId: number) {
     this.api.GetDPREmployeeReviewDetails(dprId).subscribe({
       next: (res) => {
         if (res.success && res.data) {
           const dpr = res.data as DPRReview;
-  
+
           this.WorkedHours = dpr.workedHours ?? 0;
           this.achievements = dpr.achievements ?? '';
           this.challenges = dpr.challenges ?? '';
@@ -478,11 +421,10 @@ export class MonthlyDprComponent {
           this.timeliness = dpr.scoreTimeliness ?? 0;
           this.initiative = dpr.scoreInitiative ?? 0;
           this.overallScore = dpr.scoreOverall ?? 0;
-  
+
           this.tasks = dpr.tasksList?.length ? dpr.tasksList : [];
           this.kpis = dpr.kpiList?.length ? dpr.kpiList : [];
-          this.remarksHistory = dpr.commentsList?.length ?  dpr.commentsList : []; 
-
+          this.remarksHistory = dpr.commentsList?.length ? dpr.commentsList : [];
         } else {
           console.warn('No DPR data found');
           this.tasks = [];
@@ -493,12 +435,7 @@ export class MonthlyDprComponent {
       error: (err) => {
         console.error(err);
         alert('Error loading DPR');
-      }
+      },
     });
   }
-  
-  
-
-
 }
-
